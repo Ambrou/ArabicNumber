61b5fc7645fdfad732f271206847d73557e94211
PETITGENET AMBROISE <apetitgenet@groupeastek.fr>
2023-06-20 21:13:04 +0200

    first commit

---
+++ ArabicToRomanConverter/Converter.cs
@@ -0,0 +1,49 @@
+﻿namespace ArabicToRomanConverter
+{
+	public class Converter
+	{
+		public string ToRomain(int arabicNumber)
+		{
+			string romanNumber = "";
+			// Intiution 10, 20 30 => X XX XXX
+			var rank10 = arabicNumber / 10;
+			if (rank10 != 0)
+			{
+				romanNumber += new string('X', rank10);
+				arabicNumber = arabicNumber - (10 * rank10);
+			}
+			//if (arabicNumber >= 20)
+			//{
+			//	romanNumber += "XX";
+			//	arabicNumber -= 20;
+			//}
+
+			//if (arabicNumber >= 10)
+			//{
+			//	romanNumber += "X";
+			//	arabicNumber -= 10;
+			//}
+			
+			if (arabicNumber == 9)
+			{
+				romanNumber += "IX";
+			}
+			else if (arabicNumber >= 5)
+			{
+				romanNumber += "V";
+				arabicNumber -= 5;
+			}
+			else if (arabicNumber == 4)
+			{
+				romanNumber += "IV";
+			}
+			
+			if(arabicNumber < 4)
+			{
+				romanNumber += new string('I', arabicNumber);
+			}
+			
+			return romanNumber;
+		}
+	}
+}
---
+++ ArabicToRomanConverterTestsUnit/Converter_Tests.cs
@@ -0,0 +1,53 @@
+using ArabicToRomanConverter;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ArabicToRomanConverterTests
+{
+	public class Converter_Tests
+	{
+
+		[TestCase(1, "I")]
+		[TestCase(2, "II")]
+		[TestCase(3, "III")]
+		[TestCase(4, "IV")]
+		[TestCase(5, "V")]
+		[TestCase(6, "VI")]
+		[TestCase(7, "VII")]
+		[TestCase(8, "VIII")]
+		[TestCase(9, "IX")]
+		[TestCase(10, "X")]
+		[TestCase(11, "XI")]
+		[TestCase(12, "XII")]
+		[TestCase(13, "XIII")]
+		[TestCase(14, "XIV")]
+		[TestCase(15, "XV")]
+		[TestCase(16, "XVI")]
+		[TestCase(17, "XVII")]
+		[TestCase(18, "XVIII")]
+		[TestCase(19, "XIX")]
+		[TestCase(20, "XX")]
+		[TestCase(21, "XXI")]
+		[TestCase(22, "XXII")]
+		[TestCase(23, "XXIII")]
+		[TestCase(24, "XXIV")]
+		[TestCase(25, "XXV")]
+		[TestCase(26, "XXVI")]
+		[TestCase(27, "XXVII")]
+		[TestCase(28, "XXVIII")]
+		[TestCase(29, "XXIX")]
+		[TestCase(30, "XXX")]
+		public void ConvertArabicToRomanNumber(int arabicNumber, string romanNumber)
+		{
+			// Arrange
+			Converter converter = new Converter();
+
+			// Act
+			var result = converter.ToRomain(arabicNumber);
+
+			// Assert
+			result.Should().Be(romanNumber);
+		}
+
+	}
+}
