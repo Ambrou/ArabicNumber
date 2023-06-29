3ae3ab7d3e38d8033b4208c756cc468bf03e596c
Ambrou <apetitgenet@free.fr>
2023-06-29 12:03:31 +0000

    Run 31 to 400

--- ArabicToRomanConverter/Converter.cs
+++ ArabicToRomanConverter/Converter.cs
@@ -1,49 +1,70 @@
 ﻿namespace ArabicToRomanConverter
 {
-	public class Converter
-	{
-		public string ToRomain(int arabicNumber)
-		{
-			string romanNumber = "";
-			// Intiution 10, 20 30 => X XX XXX
-			var rank10 = arabicNumber / 10;
-			if (rank10 != 0)
-			{
-				romanNumber += new string('X', rank10);
-				arabicNumber = arabicNumber - (10 * rank10);
-			}
-			//if (arabicNumber >= 20)
-			//{
-			//	romanNumber += "XX";
-			//	arabicNumber -= 20;
-			//}
+    public class Converter
+    {
+        public string ToRomain(int arabicNumber)
+        {
+            string romanNumber = "";
+            // Intuition 10, 20 30 => X XX XXX
 
-			//if (arabicNumber >= 10)
-			//{
-			//	romanNumber += "X";
-			//	arabicNumber -= 10;
-			//}
-			
-			if (arabicNumber == 9)
-			{
-				romanNumber += "IX";
-			}
-			else if (arabicNumber >= 5)
-			{
-				romanNumber += "V";
-				arabicNumber -= 5;
-			}
-			else if (arabicNumber == 4)
-			{
-				romanNumber += "IV";
-			}
-			
-			if(arabicNumber < 4)
-			{
-				romanNumber += new string('I', arabicNumber);
-			}
-			
-			return romanNumber;
-		}
-	}
+            if (arabicNumber >= 400)
+            {
+                romanNumber += "CD";
+                arabicNumber -= 400;
+            }
+          
+            var rank100 = arabicNumber / 100;
+            if (rank100 != 0)
+            {
+                romanNumber += new string('C', rank100);
+                arabicNumber = arabicNumber - (100 * rank100);
+            }
+
+            if (arabicNumber >= 90)
+            {
+                romanNumber += "XC";
+                arabicNumber -= 90;
+            }
+          
+            if (arabicNumber >= 50)
+            {
+                romanNumber += "L";
+                arabicNumber -= 50;
+            }
+          
+            if (arabicNumber >= 40)
+            {
+                romanNumber += "XL";
+                arabicNumber -= 40;
+            }
+
+            var rank10 = arabicNumber / 10;
+            if (rank10 != 0)
+            {
+                romanNumber += new string('X', rank10);
+                arabicNumber = arabicNumber - (10 * rank10);
+            }
+
+            if (arabicNumber == 9)
+            {
+                romanNumber += "IX";
+            }
+            else if (arabicNumber >= 5)
+            {
+                romanNumber += "V";
+                arabicNumber -= 5;
+            }
+            else if (arabicNumber == 4)
+            {
+                romanNumber += "IV";
+            }
+
+            if (arabicNumber < 4)
+            {
+                romanNumber += new string('I', arabicNumber);
+            }
+
+            return romanNumber;
+        }
+    }
 }
--- ArabicToRomanConverterTestsUnit/Converter_Tests.cs
+++ ArabicToRomanConverterTestsUnit/Converter_Tests.cs
@@ -4,50 +4,85 @@ using NUnit.Framework;
 
 namespace ArabicToRomanConverterTests
 {
-	public class Converter_Tests
-	{
+    public class Converter_Tests
+    {
 
-		[TestCase(1, "I")]
-		[TestCase(2, "II")]
-		[TestCase(3, "III")]
-		[TestCase(4, "IV")]
-		[TestCase(5, "V")]
-		[TestCase(6, "VI")]
-		[TestCase(7, "VII")]
-		[TestCase(8, "VIII")]
-		[TestCase(9, "IX")]
-		[TestCase(10, "X")]
-		[TestCase(11, "XI")]
-		[TestCase(12, "XII")]
-		[TestCase(13, "XIII")]
-		[TestCase(14, "XIV")]
-		[TestCase(15, "XV")]
-		[TestCase(16, "XVI")]
-		[TestCase(17, "XVII")]
-		[TestCase(18, "XVIII")]
-		[TestCase(19, "XIX")]
-		[TestCase(20, "XX")]
-		[TestCase(21, "XXI")]
-		[TestCase(22, "XXII")]
-		[TestCase(23, "XXIII")]
-		[TestCase(24, "XXIV")]
-		[TestCase(25, "XXV")]
-		[TestCase(26, "XXVI")]
-		[TestCase(27, "XXVII")]
-		[TestCase(28, "XXVIII")]
-		[TestCase(29, "XXIX")]
-		[TestCase(30, "XXX")]
-		public void ConvertArabicToRomanNumber(int arabicNumber, string romanNumber)
-		{
-			// Arrange
-			Converter converter = new Converter();
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(13, "XIII")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(16, "XVI")]
+        [TestCase(17, "XVII")]
+        [TestCase(18, "XVIII")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        [TestCase(21, "XXI")]
+        [TestCase(22, "XXII")]
+        [TestCase(23, "XXIII")]
+        [TestCase(24, "XXIV")]
+        [TestCase(25, "XXV")]
+        [TestCase(26, "XXVI")]
+        [TestCase(27, "XXVII")]
+        [TestCase(28, "XXVIII")]
+        [TestCase(29, "XXIX")]
+        [TestCase(30, "XXX")]
+        [TestCase(31, "XXXI")]
+        [TestCase(32, "XXXII")]
+        [TestCase(33, "XXXIII")]
+        [TestCase(34, "XXXIV")]
+        [TestCase(35, "XXXV")]
+        [TestCase(36, "XXXVI")]
+        [TestCase(37, "XXXVII")]
+        [TestCase(38, "XXXVIII")]
+        [TestCase(39, "XXXIX")]
+        [TestCase(40, "XL")]
+        [TestCase(41, "XLI")]
+        [TestCase(42, "XLII")]
+        [TestCase(43, "XLIII")]
+        [TestCase(44, "XLIV")]
+        [TestCase(45, "XLV")]
+        [TestCase(46, "XLVI")]
+        [TestCase(47, "XLVII")]
+        [TestCase(48, "XLVIII")]
+        [TestCase(49, "XLIX")]
+        [TestCase(50, "L")]
+        [TestCase(55, "LV")]
+        [TestCase(61, "LXI")]
+        [TestCase(77, "LXXVII")]
+        [TestCase(82, "LXXXII")]
+        [TestCase(89, "LXXXIX")]
+        [TestCase(90, "XC")]
+        [TestCase(99, "XCIX")]
+        [TestCase(100, "C")]
+        [TestCase(158, "CLVIII")]
+        [TestCase(186, "CLXXXVI")]
+        [TestCase(200, "CC")]
+        [TestCase(333, "CCCXXXIII")]
+        [TestCase(400, "CD")]
+      
 
-			// Act
-			var result = converter.ToRomain(arabicNumber);
+        public void ConvertArabicToRomanNumber(int arabicNumber, string romanNumber)
+        {
+            // Arrange
+            Converter converter = new Converter();
 
-			// Assert
-			result.Should().Be(romanNumber);
-		}
+            // Act
+            var result = converter.ToRomain(arabicNumber);
 
-	}
+            // Assert
+            result.Should().Be(romanNumber);
+        }
+
+    }
 }
